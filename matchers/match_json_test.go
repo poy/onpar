3caaec911ec5587ff3a5b4cf6cf4106462c17221
Andrew Poydence <apoydence@gmail.com>
2017-01-14 06:50:20 -0700

    Add MatchJSON matcher

---
+++ matchers/match_json_test.go
@@ -0,0 +1,27 @@
+package matchers_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/apoydence/onpar/matchers"
+)
+
+func TestMatchJSON(t *testing.T) {
+	t.Parallel()
+
+	m := matchers.MatchJSON([]byte(`{"a": 99}`))
+	v, err := m.Match(`{"a": 99}`)
+	if err != nil {
+		t.Errorf("expected %v to be nil", err)
+	}
+
+	if !reflect.DeepEqual(v, `{"a": 99}`) {
+		t.Errorf("expected %v to equal %v", v, `{"a": 99}`)
+	}
+
+	_, err = m.Match(`{"different": 99}`)
+	if err == nil {
+		t.Fatalf("expected %v to not be nil", err)
+	}
+}
