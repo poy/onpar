2aa1c59461c8af8526e70d7848c751d4972f7821
Andrew Poydence <apoydence@gmail.com>
2017-09-17 06:41:37 -0600

    Add BeNil matcher

---
+++ matchers/be_nil_test.go
@@ -0,0 +1,40 @@
+package matchers_test
+
+import (
+	"testing"
+
+	"github.com/apoydence/onpar/matchers"
+)
+
+func TestIsNil(t *testing.T) {
+	t.Parallel()
+
+	m := matchers.IsNil()
+
+	_, err := m.Match(nil)
+	if err != nil {
+		t.Error("expected err to be nil")
+	}
+
+	v, err := m.Match(42)
+	if err == nil {
+		t.Error("expected err to not be nil")
+	}
+
+	if v != 42 {
+		t.Errorf("expected %v to equal %d", v, 42)
+	}
+
+	var x map[string]string
+	_, err = m.Match(x)
+	if err != nil {
+		t.Error("expected err to be nil")
+	}
+
+	x = map[string]string{}
+
+	_, err = m.Match(x)
+	if err == nil {
+		t.Error("expected err to not be nil")
+	}
+}
